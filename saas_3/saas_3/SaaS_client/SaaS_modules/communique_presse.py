50d2adff8328c9e97f10f6fcdc4cd964d26719f8
hiba-ikbal <65920885+hiba-ikbal@users.noreply.github.com>
2021-11-11 00:42:52 -0500

    interface reft

--- saas_3/saas_3/SaaS_client/SaaS_modules/communique_presse.py
+++ saas_3/saas_3/SaaS_client/SaaS_modules/communique_presse.py
@@ -1,4 +1,4 @@
-## -*- Encoding: UTF-8 -*-
+# -*- Encoding: UTF-8 -*-
 import urllib.request
 import urllib.parse
 import json
@@ -35,53 +35,84 @@ class Vue():
 
     def creer_cadre_communique(self):
         self.root.title("Communique")
-        self.cadre_communique = Frame(self.cadreapp)
-        
+        self.cadre_gestion = Frame(self.cadreapp)
+from tkinter import font
+class Cadre_Communique(Frame):
+    def __init__(self, root):
+        root.minsize(width=700, height=500)
+        root.maxsize(width=700, height=500)
+        Frame.__init__(self, root)
+        Grid.config(self)
+        self.InfoCompagnie()
+        self.TextFrame()
+        # self.OtherInfos()
+
+    def InfoCompagnie(self):
+
+        self.infoCompagnieMethode = StringVar()
+        self.infoCompagnieMethode.set("infoCom")
+
         self.list_membre= None
+        self.infoframe = LabelFrame(self,text="Info Compagnie",height= 120,width =300)
+        self.infoframe.grid(row= 0, column=0)
+        self.infoframe.grid_propagate(0)
 
-        self.label_nom_compagnie = Label(self.cadre_communique, text="Nom de votre compagnie", font=("Arial", 12))
-        self.list_nom_compagnie = ttk.Combobox(self.cadre_communique, values=0)
+        self.label_nom_projet = Label(self.cadre_gestion, text="Nom du projet", font=("Arial", 12))
+        self.list_nom_projet = ttk.Combobox(self.cadre_gestion, values=0)
+        self.infoframe.infocom= Label(self.infoframe, text = "Compagnie :").place(x = 0,y = 30)  
+        self.infoframe.infoCom= Entry(self.infoframe, width=15,font=("Arial",16)).place(x=75,y=30)
 
-        # self.label_choix_existant = Label(self.cadre_communique, text="choisir un role existant : ", font=("Arial", 12))
+        self.label_choix_existant = Label(self.cadre_gestion, text="choisir un role existant : ", font=("Arial", 12))
+        self.traceButton = Button(self.infoframe, text="Enregistrer").place(x = 0,y = 65) 
 
-        # self.comboBox_choix_du_role = ttk.Combobox(self.cadre_communique)
+        self.comboBox_choix_du_role = ttk.Combobox(self.cadre_gestion)
+        self.tableau = ttk.Treeview(self.cadre_gestion, columns=('modules'))
+        self.btn_inscrire_modules = Button(self.cadre_gestion, text="Rafrachir", font=("Arial", 12), padx=10, pady=10, command=self.refresh)
+        self.cancelButton = Button(self.infoframe, text="Annuler").place(x = 70,y = 65) 
 
-        
-        self.tableau = ttk.Treeview(self.cadre_communique, columns=('modules'))
-        self.btn_inscrire_modules = Button(self.cadre_communique, text="Rafrachir", font=("Arial", 12), padx=10, pady=10, command=self.refresh)
-        
-        self.btn_annuler = Button(self.cadre_communique, text="Annuler", font=("Arial", 12), padx=10, pady=10)
-        self.btn_retour = Button(self.cadre_communique, text="Valider", font=("Arial", 12), padx=10, pady=10)
+        self.btn_annuler = Button(self.cadre_gestion, text="Annuler", font=("Arial", 12), padx=10, pady=10)
+        self.btn_retour = Button(self.cadre_gestion, text="Valider", font=("Arial", 12), padx=10, pady=10)
 
-        self.listbox = Listbox(self.cadre_communique, font=("Arial", 16), selectmode="multiple")
+        self.listbox = Listbox(self.cadre_gestion, font=("Arial", 16), selectmode="multiple")
 
 
 
         self.label_nom_projet.grid        (row=1, column=1, sticky='w')
         self.list_nom_projet.grid        (row=1, column=2, sticky='w')
+        self.browseButton = Button(self.infoframe, text="Ajoutrer Compagnie").place(x = 125,y = 65) 
 
         self.label_choix_existant.grid      (row=2, column=1)
         self.comboBox_choix_du_role.grid    (row=2, column=2)
         self.btn_inscrire_modules.grid      (row=2, column=3)
         self.listbox.grid (row=3, column=1, columnspan='10')
-        
+
         self.btn_annuler.grid               (row=5, column=1)
         self.btn_retour.grid                (row=5, column=2, sticky='w')
-        
-        return self.cadre_communique
+
+        return self.cadre_gestion
 
     def refresh(self):
         self.list_membre= self.list_nom_projet.get()
-        self.listbox = Listbox(self.cadre_communique, font=("Arial", 16), selectmode="multiple")
-        
+        self.listbox = Listbox(self.cadre_gestion, font=("Arial", 16), selectmode="multiple")
+
         self.listbox.grid (row=3, column=1, columnspan='10')
 
+    def TextFrame(self):
+        self.logframe = LabelFrame(self,text="Text",height= 450,width =390,padx=15)
+        self.logframe.grid_propagate(0)
+
         self.listemodules=self.parent.trouvermembres("Cineclub")
+        self.logframe.grid_rowconfigure(0,weight =1)
+        self.logframe.grid_columnconfigure(0,weight=1)
 
         entete="modules disponibles"
         for items in self.listemodules:
             self.listbox.insert(END, items)
+        xscrollbar = Scrollbar(self.logframe,orient = HORIZONTAL)
+        xscrollbar.grid(row=1, column=1, sticky=E+W,columnspan=2)
 
+        yscrollbar = Scrollbar(self.logframe)
+        yscrollbar.grid(row=0, column=3, sticky=N+S)
 
 class Modele():
     def __init__(self,parent):
@@ -89,11 +120,18 @@ class Modele():
         print(sys.argv)
         self.usager=sys.argv[2].split()
         self.inscrireusager(self.usager)
+        text = Text(self.logframe,width=50,height=60, wrap=NONE, xscrollcommand=xscrollbar.set, yscrollcommand=yscrollbar.set)
+        text.grid(row=0, column=1, columnspan=2)
+        # attach listbox to scrollbar
+        xscrollbar.config(command=text.xview)
+        yscrollbar.config(command=text.yview)
 
     def inscrireusager(self,dictinfo):
         self.nom=dictinfo[0]
         self.compagnie={"nom":dictinfo[2],
                         "id":dictinfo[4]}
+        button_1 = Button(self.logframe, text="Visualiser", command=printMessage)
+        button_1.grid(row=2,column= 1)
 
 class Controleur:
     def __init__(self):
@@ -102,25 +140,40 @@ class Controleur:
         self.modele=Modele(self)
         self.vue=Vue(self)
         self.vue.root.mainloop()
+        button_2 = Button(self.logframe, text="Envoyer", command=printMessage)
+        button_2.grid(row=2,column= 2)
 
+        self.logframe.grid(row=0,column =1,rowspan=5)
 
     def trouver_projets_par_compagnie(self):
         url = self.urlserveur+"/trouver_projet_par_compagnie"
         params ={"id": self.modele.compagnie["id"]}
         #params = {self.modele.usager[1]}
         reptext=self.appelserveur(url,params)
+    # def OtherInfos(self):
+    #     self.otherFrame = LabelFrame(self,text="Other Function",height= 400,width =300)
+    #     self.otherFrame.grid(row=4, column=0)
+    #     self.otherFrame.grid_propagate(0)
 
         mondict=json.loads(reptext)
         return mondict
+    #     OpenPreviousCaseFile = Button(self.otherFrame, text="Open previous Case File", command=printMessage,height = 4, width =25)
+    #     OpenPreviousCaseFile.grid(row=5,column= 0,pady=5)
 
     def trouvermembres(self, comp):
         url = self.urlserveur+"/trouver_membres_par_compagnie"
         params = {"comp": comp}
         reptext=self.appelserveur(url, params)
+    #     OpenPreviousTracingResult = Button(self.otherFrame, text="Open previous Tracing Result ", command=printMessage,height = 4, width =25)
+    #     OpenPreviousTracingResult.grid(row=6,column= 0,pady=5)
 
         mondict=json.loads(reptext)
         return mondict
+    #     OpenMenualbtn = Button(self.otherFrame, text="User Manual", command=printMessage,height =4, width =25)
+    #     OpenMenualbtn.grid(row=7,column= 0,pady=5)
 
+    #     AboutBtn = Button(self.otherFrame, text="About", command=printMessage,height = 4, width =25)
+    #     AboutBtn.grid(row=8,column= 0,pady=5)
 
     # fonction d'appel normalisee, utiliser par les methodes du controleur qui communiquent avec le serveur
     def appelserveur(self,url,params):
@@ -130,6 +183,12 @@ class Controleur:
         rep=urllib.request.urlopen(url, data)
         reptext=rep.read()
         return reptext
+def printMessage():
+    print("Wow this actually worked!")
+root = Tk()
+root.title("COMMUNIQUÉ DE PRESSE ")
+tif= Cadre_Communique(root)
+root.mainloop()
 
 if __name__ == '__main__':
-    c=Controleur()
+    c=Controleur() 
